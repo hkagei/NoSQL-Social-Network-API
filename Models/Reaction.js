a5e36535c9e307797dda947256a890aa1e4ce512
hkagei <k_dawg_1022@hotmail.com>
2022-07-07 00:19:50 -0600

    update Reaction.js

---
+++ Models/Reaction.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max: [280]
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+      },
+      {
+        toJSON: {
+          getters: true
+        }
+      }
+);
