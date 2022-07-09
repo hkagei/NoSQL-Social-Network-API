21de1ffb8c2506f7b4da68fe29453e918794de30
hkagei <k_dawg_1022@hotmail.com>
2022-07-09 10:57:46 -0600

    finish user.js

--- Models/User.js
+++ Models/User.js
@@ -1,21 +1,39 @@
-const { Schema, model, Types } = require('mongoose');
+const { Schema, model, Types } = require("mongoose");
 
-const UserSchema = new Schema (
-    {
-        username: {
-            type: String,
-            unique: true,
-            required: true,
-            trim: true
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true
-            
-        },
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+\@.+\..+/, "Email does not match."],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
 
-    }
-)
+const User = model("User", userSchema);
 
 module.export = User;
