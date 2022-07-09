43090c04590788c394ee53e8f9f5ca4fa57040fa
hkagei <k_dawg_1022@hotmail.com>
2022-07-09 10:36:26 -0600

    finish Thought.js

---
+++ Models/Thought.js
@@ -0,0 +1,34 @@
+const { Schema, model, Types } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const dateFormat = require('../utils/dateFormat');
+
+const thoughtSchema = new Schema (
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+
+    },
+    {
+        toJSON: {
+          getters: true
+        }
+    }
+);
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
